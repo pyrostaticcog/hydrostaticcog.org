9b91e212a7994d56df67fae19268cd5d230b6943
hydrostaticcog <hydro@hydrostaticcog.org>
2021-02-18 21:38:30 -0600

    MOOOORE BUGS

--- scripts/gh-version.js
+++ scripts/gh-version.js
@@ -1,12 +1,12 @@
-var request = new XMLHttpRequest()
+var gh2request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://raw.githubusercontent.com/Grick-Heart-Project/Grick-Heart/master/package.json', true)
+gh2request.open('GET', 'https://raw.githubusercontent.com/Grick-Heart-Project/Grick-Heart/master/package.json', true)
 
-request.onload = function () {
+gh2request.onload = function () {
     var data = JSON.parse(this.response)
     document.getElementById('ghversion-number').innerHTML = data.version;
 }
 
 // Send request
-request.send()
+gh2request.send()
--- scripts/git-issues.js
+++ scripts/git-issues.js
@@ -1,9 +1,9 @@
-var request = new XMLHttpRequest()
+var gh1request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.github.com/repos/Grick-Heart-Project/Grick-Heart/issues/23', true)
+gh1request.open('GET', 'https://api.github.com/repos/Grick-Heart-Project/Grick-Heart/issues/23', true)
 
-request.onload = function () {
+gh1request.onload = function () {
     var data = JSON.parse(this.response)
     document.getElementById('issue-name').innerHTML = data.title;
     document.getElementById('issue-number').innerHTML = data.number;
