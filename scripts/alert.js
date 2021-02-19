cbb25273b908e89f4266fc249591a8ab37b2f440
hydrostaticcog <hydro@hydrostaticcog.org>
2021-02-18 21:22:53 -0600

    update alert system

---
+++ scripts/alert.js
@@ -0,0 +1,28 @@
+let request = new XMLHttpRequest()
+
+request.open('GET', 'http://hydrostaticcog/alert.json', true)
+
+request.onload = function() {
+    var alertFile = JSON.parse(this.response)
+    console.log(alertFile.onAlert)
+    if (alertFile.onAlert) {
+        document.getElementById('alerts').innerHTML = 'WE ARE CURRENTLY EXPERIENCING SOME OUTAGES. SOME SERVICES MAY BE AFFECTED'
+        var alertBox = document.querySelector('.alert')
+        alertBox.style.padding = '5px';
+        alertBox.style.backgroundColor = 'rgb(255, 0, 0)';
+        alertBox.style.color = 'white';
+        alertBox.style.marginBottom = '10px';
+        var alertLink = document.createElement('a');
+        var linkText = document.createTextNode('Status Page');
+        alertLink.appendChild(linkText)
+        alertLink.title = 'Status Page';
+        alertLink.href = 'https://status.hydrostaticcog.org/outages/' + alertFile['alertID'];
+        document.getElementById('alertButton').appendChild(alertLink)
+        document.getElementById('alert1').style.display = 'block';
+    } else {
+        return
+    }
+
+}
+
+request.send()
