40fa75f958171a50d9ee0dca0a6a370c5cee2621
hydrostaticcog <hydro@hydrostaticcog.org>
2021-02-18 21:28:03 -0600

    fix bug 2.0

--- scripts/alert.js
+++ scripts/alert.js
@@ -1,6 +1,6 @@
 let request = new XMLHttpRequest()
 
-request.open('GET', 'https://hydrostaticcog/alert.json', true)
+request.open('GET', 'https://hydrostaticcog.org/alert.json', true)
 
 request.onload = function() {
     var alertFile = JSON.parse(this.response)
